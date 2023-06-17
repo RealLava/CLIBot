515200c51fd5d0abb9143658c60c040e3f147dc6
Lava <69821200+RealLava@users.noreply.github.com>
2023-06-17 02:52:44 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,4 @@
 @echo off
-set /p numBots="Enter the number of bots to start: "
 set /p serverHost="Enter the server host: " 
-echo Starting %numBots% bot(s)...
-for /l %%i in (1,1,%numBots%) do (
-    start cmd /k node --max-old-space-size=512 . %serverHost%
-)
-echo Clibot was loaded.
+start cmd /k node --max-old-space-size=512 . %serverHost%
 exit
